1561d2a41ae68ed1a74ac1f9b0e5714c4758f22a
nick ackerman <nckackerman@gmail.com>
2018-05-25 17:47:00 -0700

    Adding Domain Name to ec2.yml

--- infrastructure/start.sh
+++ infrastructure/start.sh
@@ -5,23 +5,11 @@ killall -9 java
 
 if [[ ${DEPLOYMENT_GROUP_NAME} =~ "WebServer" ]]
 then
-    # Uncomment once we get our own domain name and can fix nginx routing to not depend on ec2 instance ips
-    # echo "Stopping nginx"
-    # service nginx stop
+    echo "Stopping nginx"
+    service nginx stop
+    sudo cp /var/www/streamercontracts/nginx.conf /etc/nginx/nginx.conf
     echo "Starting nginx StreamerContracts-WebServer. Service wont work until you ssh onto this instance and update nginx.conf."
-    # nginx doesnt know where to route web and api traffic and currently has placeholders
-    # - REPLACE_WITH_WEB_HOST, REPLACE_WITH_WEB_PORT, REPLACE_WITH_API_HOST, REPLACE_WITH_API_PORT
-    # CodeDeploy does not have access to ENVIRONMENT_VARIABLES. As such, until we have a dedicated hostname, the best
-    # approach we have for configuring nginx is to ssh onto the WebServer ec2 instance and run the following commands manually:
-
-    # sudo cp /var/www/streamercontracts/nginx.conf /etc/nginx/nginx.conf
-    # sudo sed -i "s|REPLACE_WITH_WEB_HOST|$WEB_HOST|g" /etc/nginx/nginx.conf
-    # sudo sed -i "s|REPLACE_WITH_WEB_PORT|$WEB_PORT|g" /etc/nginx/nginx.conf
-    # sudo sed -i "s|REPLACE_WITH_API_HOST|$API_HOST|g" /etc/nginx/nginx.conf
-    # sudo sed -i "s|REPLACE_WITH_API_PORT|$API_PORT|g" /etc/nginx/nginx.conf
-    # sudo service nginx start &
-
-    exit $?
+    sudo service nginx start & exit $?
 elif [[ ${DEPLOYMENT_GROUP_NAME} =~ "Web" ]]
 then
     echo "Starting StreamerContracts-Web SpringBoot Application"
