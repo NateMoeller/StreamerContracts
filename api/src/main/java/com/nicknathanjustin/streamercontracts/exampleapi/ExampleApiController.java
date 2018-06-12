98443059cf8c0ebe98b746c7de863eb8ebd24641
nick ackerman <nckackerman@gmail.com>
2018-06-11 20:44:40 -0700

    adding loggin to example api

--- api/src/main/java/com/nicknathanjustin/streamercontracts/exampleapi/ExampleApiController.java
+++ api/src/main/java/com/nicknathanjustin/streamercontracts/exampleapi/ExampleApiController.java
@@ -2,6 +2,7 @@ package com.nicknathanjustin.streamercontracts.exampleapi;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
+@Slf4j
 public class ExampleApiController {
 
     @NonNull private final ExampleService exampleService;
@@ -26,6 +28,7 @@ public class ExampleApiController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String getExample() {
+        log.info("Sample log message to test logging. Check cloudwatch logs to verify this works, {}", System.nanoTime());
         return exampleService.getExampleValue(UUID.randomUUID());
     }
 
