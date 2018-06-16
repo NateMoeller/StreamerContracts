9aabd77e3e0b9e3c26368b88fe6e30711b636b63
nick ackerman <nckackerman@gmail.com>
2018-06-16 09:41:44 -0700

    adding nginx logging to aws cloudwatch

--- dev_ops/infrastructure/code_deploy/start.sh
+++ dev_ops/infrastructure/code_deploy/start.sh
@@ -5,18 +5,23 @@ killall -9 java
 
 if [[ ${DEPLOYMENT_GROUP_NAME} =~ "WebServer" ]]
 then
+    sudo cp /var/www/streamercontracts/awslogs_nginx.conf /etc/awslogs/awslogs.conf
+    sudo service awslogs start
+    sudo chkconfig awslogs on
     echo "Stopping nginx"
     service nginx stop
     sudo cp /var/www/streamercontracts/nginx-beta.conf /etc/nginx/nginx.conf
     echo "Starting nginx StreamerContracts-WebServer."
     sudo service nginx start
+
 elif [[ ${DEPLOYMENT_GROUP_NAME} =~ "Api" ]]
 then
     echo "Starting StreamerContracts-Api SpringBoot Application"
-    sudo cp /var/www/streamercontracts/awslogs.conf /etc/awslogs/awslogs.conf
+    sudo cp /var/www/streamercontracts/awslogs_api.conf /etc/awslogs/awslogs.conf
     sudo service awslogs start
     sudo chkconfig awslogs on
-    java -Dspring.profiles.active=beta -jar /var/www/streamercontracts/api-1.0-SNAPSHOT.jar > /var/log/streamer-contracts.txt 2>&1 &
+    currentDate=`date '+%Y_%m_%d_%H'`;
+    java -Dspring.profiles.active=beta -jar /var/www/streamercontracts/api-1.0-SNAPSHOT.jar > /var/log/streamer-contracts.$currentDate.txt 2>&1 &
     exit $?
 else
     echo "Invalid deployment group name '${DEPLOYMENT_GROUP_NAME}', no role found " 2>&1 &
