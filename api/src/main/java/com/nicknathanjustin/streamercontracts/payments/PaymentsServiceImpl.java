7d7a593a1e1a6724505527e64e04738381e60b44
nick ackerman <nckackerman@gmail.com>
2018-06-24 22:05:41 -0700

    Fixing issue where paypal API calls made after the first call fail. Fixed by creating a new APIContext per paypal API call. See this issue: https://github.com/paypal/PayPal-PHP-SDK/issues/157

--- api/src/main/java/com/nicknathanjustin/streamercontracts/payments/PaymentsServiceImpl.java
+++ api/src/main/java/com/nicknathanjustin/streamercontracts/payments/PaymentsServiceImpl.java
@@ -17,16 +17,15 @@ public class PaymentsServiceImpl implements PaymentsService {
     private static final String CLIENT_ID = "AV_VKTQQlMAB8Fq58souv8_CazzqdHZS5pDF_H9L4eZIIH65I6LjvoSQVBHmgCk9-uJwtpsKxfOX9bop";
     private static final String CLIENT_SECRET = "EBJi9gvGJrKBwWDmIchrgLG2gWUQRAEjbM9jQyojkH16dXm0QUZfZnpMiZPTB1hXF8cFJVxnhUW-te5Q";
     private static final String MODE = "sandbox";
-    private static final APIContext API_CONTEXT = new APIContext(CLIENT_ID, CLIENT_SECRET, MODE);
 
     @Override
     public Optional<Payment> executePayment(@NonNull final String paymentId) {
         log.info("executing payment for paymentId: {}", paymentId);
         try {
-            final Payment payment = Payment.get(API_CONTEXT, paymentId);
+            final Payment payment = Payment.get(getAPIContext(), paymentId);
             final PaymentExecution paymentExecution = new PaymentExecution();
             paymentExecution.setPayerId(payment.getPayer().getPayerInfo().getPayerId());
-            return Optional.of(payment.execute(API_CONTEXT, paymentExecution));
+            return Optional.of(payment.execute(getAPIContext(), paymentExecution));
         } catch (PayPalRESTException e) {
             log.error("error executing payment for paymentId: {}. full error: {}", paymentId, e.toString());
         }
@@ -37,13 +36,13 @@ public class PaymentsServiceImpl implements PaymentsService {
     public boolean capturePayment(@NonNull final String authorizationTransactionId) {
         log.info("capturing authorizationTransaction for authorizationTransactionId: {}", authorizationTransactionId);
         try {
-            final Authorization authorization = Authorization.get(API_CONTEXT, authorizationTransactionId);
+            final Authorization authorization = Authorization.get(getAPIContext(), authorizationTransactionId);
             final Capture capture = new Capture();
             // Note: we cannot reuse the existing Amount from authorization.getAmount(). We need to create a new Amount
             // object to match PayPals API
             final Amount amount = new Amount(authorization.getAmount().getCurrency(), authorization.getAmount().getTotal());
             capture.setAmount(amount);
-            return authorization.capture(API_CONTEXT, capture).getState().equals("completed");
+            return authorization.capture(getAPIContext(), capture).getState().equals("completed");
         } catch (PayPalRESTException e) {
             log.error("error capturing authorizationTransaction for authorizationTransactionId: {}. full error: {}",
                     authorizationTransactionId, e.toString());
@@ -55,12 +54,17 @@ public class PaymentsServiceImpl implements PaymentsService {
     public boolean voidPayment(@NonNull final String authorizationTransactionId) {
         log.info("voiding authorizationTransaction for authorizationTransactionId: {}", authorizationTransactionId);
         try {
-            final Authorization authorization = Authorization.get(API_CONTEXT, authorizationTransactionId);
-            return authorization.doVoid(API_CONTEXT).getState().equals("voided");
+            final Authorization authorization = Authorization.get(getAPIContext(), authorizationTransactionId);
+            return authorization.doVoid(getAPIContext()).getState().equals("voided");
         } catch (PayPalRESTException e) {
             log.error("error voiding authorizationTransaction for authorizationTransactionId: {}. full error: {}",
                     authorizationTransactionId, e.toString());
         }
         return false;
+    }
+
+    private APIContext getAPIContext() {
+        //NOTE: a new APIContext is needed for each paypal API call
+        return new APIContext(CLIENT_ID, CLIENT_SECRET, MODE);
     }
 }
