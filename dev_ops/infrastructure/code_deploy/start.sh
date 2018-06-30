c415d916753fd14d4b8ff6bd929b4688021cbd84
nick ackerman <nckackerman@gmail.com>
2018-06-30 13:37:44 -0700

    restart awslogs after new logfile is created

--- dev_ops/infrastructure/code_deploy/start.sh
+++ dev_ops/infrastructure/code_deploy/start.sh
@@ -6,22 +6,22 @@ killall -9 java
 if [[ ${DEPLOYMENT_GROUP_NAME} =~ "WebServer" ]]
 then
     sudo cp /var/www/streamercontracts/awslogs_nginx.conf /etc/awslogs/awslogs.conf
-    sudo service awslogs restart
     sudo chkconfig awslogs on
     echo "Stopping nginx"
     service nginx stop
     sudo cp /var/www/streamercontracts/nginx-beta.conf /etc/nginx/nginx.conf
     echo "Starting nginx StreamerContracts-WebServer."
     sudo service nginx start
+    sudo service awslogs restart
 
 elif [[ ${DEPLOYMENT_GROUP_NAME} =~ "Api" ]]
 then
     echo "Starting StreamerContracts-Api SpringBoot Application"
     sudo cp /var/www/streamercontracts/awslogs_api.conf /etc/awslogs/awslogs.conf
-    sudo service awslogs restart
     sudo chkconfig awslogs on
     currentDate=`date '+%Y_%m_%d_%H_%M'`;
-    java -Dspring.profiles.active=beta -jar /var/www/streamercontracts/api-1.0-SNAPSHOT.jar > /var/log/streamer-contracts.log.$currentDate 2>&1 &
+    java -Dspring.profiles.active=beta -jar /var/www/streamercontracts/api-1.0-SNAPSHOT.jar > /var/log/streamer-contracts.log.$currentDate 2>&1
+    sudo service awslogs restart
     exit $?
 else
     echo "Invalid deployment group name '${DEPLOYMENT_GROUP_NAME}', no role found " 2>&1 &
