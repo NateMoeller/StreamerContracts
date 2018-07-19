9afdc93534caa63650a944b0008617bc3c3108e1
Nathan Moeller <ndmoeller@wisc.edu>
2018-07-18 21:50:33 -0700

    Fixing create table script

--- dev_ops/sql/CreateSchema.sql
+++ dev_ops/sql/CreateSchema.sql
@@ -1,10 +1,10 @@
 -- Creates all of the tables in the database
 -- Need to use Linux style slashes, even on Windows
 
-\i './Tables/CreateUsers.sql'
-\i './Tables/CreateContracts.sql'
-\i './Tables/CreateContractDonations.sql'
-\i './Tables/CreatePolls.sql'
-\i './Tables/CreateVotes.sql'
-\i './Tables/CreateFeaturedContracts.sql'
-\i './Tables/CreatePaymentInfo.sql'
+\i 'dev_ops/sql/Tables/CreateUsers.sql'
+\i 'dev_ops/sql/Tables/CreateContracts.sql'
+\i 'dev_ops/sql/Tables/CreateContractDonations.sql'
+\i 'dev_ops/sql/Tables/CreatePolls.sql'
+\i 'dev_ops/sql/Tables/CreateVotes.sql'
+\i 'dev_ops/sql/Tables/CreateFeaturedContracts.sql'
+\i 'dev_ops/sql/Tables/CreatePaymentInfo.sql'
