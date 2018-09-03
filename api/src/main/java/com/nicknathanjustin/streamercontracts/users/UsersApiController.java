da800be95e3a244692d9253ad0f49859941c7852
nick ackerman <nckackerman@gmail.com>
2018-09-03 14:01:00 -0700

    fixing NPE

--- api/src/main/java/com/nicknathanjustin/streamercontracts/users/UsersApiController.java
+++ api/src/main/java/com/nicknathanjustin/streamercontracts/users/UsersApiController.java
@@ -3,6 +3,7 @@ package com.nicknathanjustin.streamercontracts.users;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.Nullable;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,14 +29,16 @@ public class UsersApiController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public void registerUserIfNotExists(@NonNull final HttpServletResponse response, @NonNull final OAuth2Authentication auth) throws IOException {
-        final TwitchUser twitchUser = TwitchUser.createTwitchUser(auth);
-        final UserModel user = userService.getUser(twitchUser.getDisplayName());
-        if (user != null) {
-            userService.login(user);
-        }
-        else {
-            userService.createUser(twitchUser.getDisplayName());
+    public void registerUserIfNotExists(@NonNull final HttpServletResponse response, @Nullable final OAuth2Authentication auth) throws IOException {
+        if(auth != null) {
+            final TwitchUser twitchUser = TwitchUser.createTwitchUser(auth);
+            final UserModel user = userService.getUser(twitchUser.getDisplayName());
+            if (user != null) {
+                userService.login(user);
+            }
+            else {
+                userService.createUser(twitchUser.getDisplayName());
+            }
         }
 
         response.sendRedirect(frontEndUrl + "/profile");
