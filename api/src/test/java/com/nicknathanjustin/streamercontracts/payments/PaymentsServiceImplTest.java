e8aa44f627f2a99d9d892f722c3215c68b146bbf
nick ackerman <nckackerman@gmail.com>
2018-09-24 19:35:38 -0700

    reverting previous change

--- api/src/test/java/com/nicknathanjustin/streamercontracts/payments/PaymentsServiceImplTest.java
+++ api/src/test/java/com/nicknathanjustin/streamercontracts/payments/PaymentsServiceImplTest.java
@@ -11,7 +11,7 @@ public class PaymentsServiceImplTest {
     @InjectMocks
     private PaymentsServiceImpl paymentsService;
 
-    @Test
+    @Test(expected = NullPointerException.class)
     public void executePayment_nullInput_throwsException() {
         paymentsService.executePayment(null);
     }
