cffc9cf03ccb3455ba28756dfaaabff61dc58e12
nick ackerman <nckackerman@gmail.com>
2018-09-24 19:30:02 -0700

    pushin failing unit test to check build logic

--- api/src/test/java/com/nicknathanjustin/streamercontracts/payments/PaymentsServiceImplTest.java
+++ api/src/test/java/com/nicknathanjustin/streamercontracts/payments/PaymentsServiceImplTest.java
@@ -11,7 +11,7 @@ public class PaymentsServiceImplTest {
     @InjectMocks
     private PaymentsServiceImpl paymentsService;
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void executePayment_nullInput_throwsException() {
         paymentsService.executePayment(null);
     }
