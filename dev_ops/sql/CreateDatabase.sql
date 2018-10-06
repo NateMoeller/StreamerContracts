bc05d3735cbe0e64b71a761f01b011a10a2a78d6
Justin Moeller <justin.g.moeller@gmail.com>
2018-10-06 13:49:39 -0500

    Change SQL Scripts to setup Local DB (#24)
    * Changed the scripts to setup a db admin
    
    * Drop privileges with user

--- dev_ops/sql/CreateDatabase.sql
+++ dev_ops/sql/CreateDatabase.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE StreamerContractsDB;
+
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM application_user;
+
+REVOKE ALL PRIVILEGES ON DATABASE streamercontractsdb FROM application_user;
+
+DROP USER application_user;
+
+CREATE USER application_user WITH ENCRYPTED PASSWORD 'password';
+
+GRANT ALL PRIVILEGES ON DATABASE streamercontractsdb TO application_user;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO application_user;
