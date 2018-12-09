ba9ff54c4bb39422a73a4c7b11c625a75b89b09f
Nathan Moeller <n.moeller18@gmail.com>
2018-12-09 13:54:58 -0800

    Updating java doc

--- api/src/main/java/com/nicknathanjustin/streamercontracts/twitch/TwitchService.java
+++ api/src/main/java/com/nicknathanjustin/streamercontracts/twitch/TwitchService.java
@@ -8,7 +8,8 @@ public interface TwitchService {
     /**
      * Get the top games from the twitch api
      *
-     * @return A List of top games
+     * @return A Map with 2 fields, data and pagination.
+     * data contains a list of games, and pagination contains a cursor value to specify the starting point of the next set of results.
      */
     Map<String, List<Map<String, Object>>> getTopGames();
 }
